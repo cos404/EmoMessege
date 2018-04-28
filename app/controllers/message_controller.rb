ffe48118b871c461d9998bd8bdfa34d2668849b0
cos404 <22division7@gmail.com>
2018-04-28 11:37:37 +0300

    Add: checking messages on duplicate

--- app/controllers/message_controller.rb
+++ app/controllers/message_controller.rb
@@ -1,10 +1,11 @@
 class MessageController < ApplicationController
   before_action :set_params, only: [:create]
-
+  REG_DELAY = 10
 
   def create
     return if !authorized?(message_params[:token])
     return if !validates_params?
+    return if duplicate_message?
     @ids = []
 
     process_message(@telegram, :telegram)
@@ -19,6 +20,13 @@ class MessageController < ApplicationController
 
   def message_params
     params.permit(:token, :message, :telegram, :whats_up, :viber, telegram: [], whats_up: [], viber: [])
+  end
+
+  def set_params
+    @message = message_params[:message]
+    @telegram = message_params[:telegram]
+    @whats_up = message_params[:whats_up]
+    @viber = message_params[:viber]
   end
 
   def process_message(recipient, service)
@@ -56,13 +64,24 @@ class MessageController < ApplicationController
       return false
     end
 
-    return true
+    true
   end
 
-  def set_params
-    @message = message_params[:message]
-    @telegram = message_params[:telegram]
-    @whats_up = message_params[:whats_up]
-    @viber = message_params[:viber]
+  def duplicate_message?
+    recipients = [@telegram, @whats_app, @viber].flatten.select {|e| not e.nil?}
+
+    duplicate_message = Message.select(:id).where(
+      user_id: @user.id,
+      recipient: recipients,
+      created_at: Time.now-REG_DELAY..Time.now,
+      message: @message)
+
+    if recipients.length == duplicate_message.length
+      render json: {duplicate_message: "Duplicate message! Wait #{REG_DELAY} seconds or send another message."}, status: :conflict
+      return true
+    end
+
+    false
   end
+
 end
